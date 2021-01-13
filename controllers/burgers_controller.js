504e1912c6106991c2dd0e1d9933b8707c7abd49
Asli <aslinurkeklikci@yahoo.com>
2021-01-13 18:44:21 -0500

    added orm, views

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,34 @@
+var express = require("express");
+
+var router = express.Router();
+var burger = require("../models/burger.js");
+
+// get route
+router.get("/", function(req, res) {
+  res.redirect("/burgers");
+});
+
+router.get("/burgers", function(req, res) {
+
+  burger.all(function(burgerData) {
+    res.render("index", { burger_data: burgerData });
+  });
+});
+
+// post
+router.post("/burgers/create", function(req, res) {
+  burger.create(req.body.burger_name, function(result) {
+    console.log(result);
+    res.redirect("/");
+  });
+});
+
+// put route 
+router.put("/burgers/:id", function(req, res) {
+  burger.update(req.params.id, function(result) {
+    console.log(result);
+    res.sendStatus(200);
+  });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,10 +3,8 @@ var express = require("express");
 var PORT = process.env.PORT || 8000;
 var app = express();
 
-// Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
-// Parse application body
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
